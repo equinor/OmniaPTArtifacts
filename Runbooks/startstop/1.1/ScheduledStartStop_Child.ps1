2abff145a72e2a9c6029966cbc49d75c45d33602
Tor Ivar <torivara@gmail.com>
2021-12-20 09:02:40 +0100

    Changed to tag include logic

--- Runbooks/startstop/1.1/ScheduledStartStop_Child.ps1
+++ Runbooks/startstop/1.1/ScheduledStartStop_Child.ps1
@@ -12,8 +12,8 @@ param(
     [string]$VMName = $(throw 'Value for VMName is missing'),
     [String]$Action = $(throw 'Value for Action is missing'),
     [String]$ResourceGroupName = $(throw 'Value for ResourceGroupName is missing'),
-    [string]$ExcludedTagName = 'OmniaPT_AutoStartStopDisabled',
-    [String]$ExcludedTagValue = 'True'
+    [string]$IncludedTagName = 'OmniaPT_AutoStartStopEnabled',
+    [String]$IncludedTagValue = 'True'
 )
 
 [string] $FailureMessage = 'Failed to execute the command'
@@ -38,62 +38,65 @@ do {
         Write-Output 'Successfully logged into Azure subscription using Az cmdlets...'
 
         Write-Output "VM action is : $($Action)"
-
         $vmTags = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName | Select-Object Tags
 
-        if ($vmTags.Tags[$ExcludedTagName] -eq $ExcludedTagValue) {
-            Write-Output "Virtual Machine $($VMName) excluded by tag."
-            $RetryFlag = $false
-        } elseif ($Action.Trim().ToLower() -eq 'stop') {
-            Write-Output "Stopping the Virtual Machine : $($VMName)"
+        if ($Action.Trim().ToLower() -eq 'stop') {
 
-            $Status = Stop-AzVM -Name $VMName -ResourceGroupName $ResourceGroupName -Force
+            if ($vmTags.Tags[$IncludedTagName] -eq $IncludedTagValue) {
+                Write-Output "Virtual Machine $($VMName) included by tag."
+                Write-Output "Stopping the Virtual Machine : $($VMName)"
+                $Status = Stop-AzVM -Name $VMName -ResourceGroupName $ResourceGroupName -Force
+            } else {
+                Write-Output "Virtual Machine $($VMName) not included by tag."
+                $Status = 'NotIncluded'
+            }
 
             if ($null -eq $Status) {
                 Write-Output "Error occurred while stopping the Virtual Machine $($VMName) hence retrying..."
-
                 if ($Attempt -gt $RetryCount) {
                     Write-Output "Reached the max $RetryCount retry attempts so please resubmit the job later..."
-
                     $RetryFlag = $false
                 } else {
                     Write-Output "[$Attempt/$RetryCount] Retrying in $TimeoutInSecs seconds..."
-
                     Start-Sleep -Seconds $TimeoutInSecs
-
                     $Attempt = $Attempt + 1
-
                     $RetryFlag = $true
                 }
             } else {
-                Write-Output "Successfully stopped the Virtual Machine : $($VMName)"
-
+                if ($Status -eq 'NotIncluded') {
+                    Write-Output "Virtual Machine $($VMName) not stopped because it was not included by tag."
+                } else {
+                    Write-Output "Successfully stopped the Virtual Machine : $($VMName)"
+                }
                 $RetryFlag = $false
             }
         } elseif ($Action.Trim().ToLower() -eq 'start') {
-            Write-Output "Starting the Virtual Machine : $($VMName)"
-
-            $Status = Start-AzVM -Name $VMName -ResourceGroupName $ResourceGroupName
+            if ($vmTags.Tags[$IncludedTagName] -eq $IncludedTagValue) {
+                Write-Output "Virtual Machine $($VMName) included by tag."
+                Write-Output "Starting the Virtual Machine : $($VMName)"
+                $Status = Start-AzVM -Name $VMName -ResourceGroupName $ResourceGroupName
+            } else {
+                Write-Output "Virtual Machine $($VMName) not included by tag."
+                $Status = 'NotIncluded'
+            }
 
             if ($null -eq $Status) {
                 Write-Output "Error occurred while starting the Virtual Machine $($VMName) hence retrying..."
-
                 if ($Attempt -gt $RetryCount) {
                     Write-Output "Reached the max $RetryCount retry attempts so please resubmit the job later..."
-
                     $RetryFlag = $false
                 } else {
                     Write-Output "[$Attempt/$RetryCount] Retrying in $TimeoutInSecs seconds..."
-
                     Start-Sleep -Seconds $TimeoutInSecs
-
                     $Attempt = $Attempt + 1
-
                     $RetryFlag = $true
                 }
             } else {
-                Write-Output "Successfully started the Virtual Machine : $($VMName)"
-
+                if ($Status -eq 'NotIncluded') {
+                    Write-Output "Virtual Machine $($VMName) not stopped because it was not included by tag."
+                } else {
+                    Write-Output "Successfully started the Virtual Machine : $($VMName)"
+                }
                 $RetryFlag = $false
             }
         }

0108d2621883004e2f0d87529a66e48ad1cd0e78
Tor Ivar <torivara@gmail.com>
2021-12-30 11:54:51 +0100

    Added synopsis

--- Scripts/GuestConfiguration/CreateAndPublishPackage.ps1
+++ Scripts/GuestConfiguration/CreateAndPublishPackage.ps1
@@ -1,4 +1,44 @@
-﻿[CmdletBinding()]
+﻿<#
+.SYNOPSIS
+ Script for creating and publishing a Guest Configuration Package
+.DESCRIPTION
+ This script will create a Guest Configuration package based on an existing MOF DSC definition.
+ If the relevant booleans and information is provided, it will also attempt to publish a config package,
+ and create a Guest Configuration Policy.
+.EXAMPLE
+Will create a package and publish it to a specified storage account
+.\CreateAndPubilshPackage.ps1 -ConfigurationName 'GuestConfig_Baseline' `
+    -ConfigurationType 'AuditAndSet' `
+    -ConfigurationFilePath '.\configurations\configurationfile.mof' `
+    -PublishPackage $true `
+    -StorageAccountName 'mystorageaccountname' `
+    -ResourceGroupName 'MyResourceGroupName'
+
+Will create a package, publish it, and create an Azure Policy for assigning it
+$PolicyParameters = @{
+    "PolicyId" = "1111-22222-3333-44444-55555"
+        "DisplayName" = "MyDisplayName"
+        "Description" = "MyDescription"
+        "Path" = ".\myPolicyFiles"
+        "Platform" = "Windows/Linux"
+        "Version" = "1.0.0.0"
+        "Mode" = "ApplyAndMonitor/ApplyAndAutoCorrect/Audit"
+        "Tag" = @{"TagName"="TagValue"}
+}
+.\CreateAndPubilshPackage.ps1 -ConfigurationName 'GuestConfig_Baseline' `
+    -ConfigurationType 'AuditAndSet' `
+    -ConfigurationFilePath '.\configurations\configurationfile.mof' `
+    -PublishPackage $true `
+    -StorageAccountName 'mystorageaccountname' `
+    -ResourceGroupName 'MyResourceGroupName' `
+    -PolicyParameters $PolicyParameters
+.NOTES
+Version History
+v0.1   - Initial Draft
+
+#>
+
+[CmdletBinding()]
 param (
     [Parameter(Mandatory = $false)]
     [String]$ConfigurationName = 'MyConfig',
@@ -7,8 +47,6 @@ param (
     [String]$ConfigurationType = 'Audit',
     [Parameter(Mandatory = $true)]
     [String]$ConfigurationFilePath,
-    [Parameter(Mandatory = $true)]
-    [String]$Version,
     [Parameter(Mandatory = $false)]
     [bool]$PublishPackage = $false,
     [Parameter(Mandatory = $false)]
@@ -18,9 +56,7 @@ param (
     [Parameter(Mandatory = $false)]
     [String]$ResourceGroupName,
     [Parameter(Mandatory = $false)]
-    [hashtable]$PolicyParameters = @{},
-    [Parameter(Mandatory = $false)]
-    [bool]$PublishPolicy = $false
+    [hashtable]$PolicyParameters = @{}
 )
 
 if (!(Test-Path -Path $ConfigurationFilePath)) {
@@ -34,14 +70,14 @@ $package = New-GuestConfigurationPackage `
     -Type $ConfigurationType `
     -Force
 
-if ($PublishPackage) {
-    Publish-GuestConfigurationPackage -Path $package.Path -ResourceGroupName $ResourceGroupName -StorageAccountName $StorageAccountName -StorageContainerName $StorageContainerName -Force | ForEach-Object ContentUri
+if ($PublishPackage -and $StorageAccountName -and $ResourceGroupName) {
+    $ContentUri = Publish-GuestConfigurationPackage -Path $package.Path -ResourceGroupName $ResourceGroupName -StorageAccountName $StorageAccountName -StorageContainerName $StorageContainerName -Force | ForEach-Object ContentUri
 }
 
-if ($PolicyParameters.Count -gt 0) {
+if ($PolicyParameters.Count -gt 0 -and $contentUri) {
     New-GuestConfigurationPolicy `
         -PolicyId $PolicyParameters['PolicyId'] `
-        -ContentUri $PolicyParameters['ContentUri'] `
+        -ContentUri $ContentUri `
         -DisplayName $PolicyParameters['DisplayName'] `
         -Description $PolicyParameters['Description'] `
         -Path $PolicyParameters['Path'] `
